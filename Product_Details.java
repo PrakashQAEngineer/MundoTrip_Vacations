cea6e4f2d21dbfac6a80e83891ea1b43e0e02dba
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2025-04-28 16:29:36 +0530

    Product_Details.java

--- Product_Details.java
+++ Product_Details.java
@@ -14,53 +14,64 @@ public class Product_Details
 	{
        WebDriver driver = new ChromeDriver();
        driver.manage().window().maximize();
-       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
        
-       //Open ing of the Mundo Trip
-       driver.get("https://www.mundotrip.com/en-us");
        
-       //click on the vacations
-       driver.findElement(By.xpath("//a[@rel = '#holidays']")).click();
-       
-       //select Dubai from the dropdown
-       Thread.sleep(3000);
-       WebElement wb1 = driver.findElement(By.xpath("//select[@id = 'destinationHoliday']"));
-       
-       Select sc = new Select(wb1);
-       sc.selectByVisibleText("Dubai");
-       
-       //click on the Chat Icon
-       Thread.sleep(3000);
-      driver.findElement(By.className("cc-chat-bubble")).click();
-      
-      //Month Selection
-      WebElement wb2 = driver.findElement(By.xpath("//select[@id = 'monthDropdown']"));
-      Select scc = new Select(wb2);
-      scc.selectByVisibleText("June'25");
-      
-      //select on Search Button
-      driver.findElement(By.id("searchHoliday")).click();
-      
-   // selecting the deal "Discover the Magic of Dubai & Abu Dhabi: A Luxury Tour Experience"
-      
-       List<WebElement> wb11 = driver.findElements(By.className("holilist-content"));
-       //System.out.println(wb1.getSize());
-       for(int i=0; i<wb11.size();i++)
-       {
-    	   //System.out.println("value of i: "+i);
-    	   String hol = wb11.get(i).getText();
-    	   if(hol.contains("Discover the Magic of Dubai & Abu Dhabi: A Luxury Tour Experience"))
-    	   {
-    		   //Thread.sleep(5000);
-    		   
-    		   WebElement el = driver.findElements(By.xpath("//button[@type='submit']")).get(i);
-    		   Actions act = new Actions(driver);
-    		   act.moveToElement(el).click().perform();
-    		   break;
-    	   }
-    	   //System.out.println(hol);
-    	   
-       }
+       homePage(driver);
+       vacationListing(driver);   
+	}
+	
+	public static void homePage(WebDriver driver) throws InterruptedException
+	{
+		//Opening of the Mundo Trip
+	       driver.get("https://www.mundotrip.com/en-us");
+	       
+	     //click on the vacations
+	       driver.findElement(By.xpath("//a[@rel = '#holidays']")).click();
+	       
+	       //select Dubai from the dropdown
+	       Thread.sleep(3000);
+	       WebElement wb1 = driver.findElement(By.xpath("//select[@id = 'destinationHoliday']"));
+	       
+	       Select sc = new Select(wb1);
+	       sc.selectByVisibleText("Dubai");
+	       
+	       //click on the Chat Icon
+	       Thread.sleep(3000);
+	      driver.findElement(By.className("cc-chat-bubble")).click();
+	      
+	      //Month Selection
+	      WebElement wb2 = driver.findElement(By.xpath("//select[@id = 'monthDropdown']"));
+	      Select scc = new Select(wb2);
+	      scc.selectByVisibleText("June'25");
+	      
+	      //select on Search Button
+	      driver.findElement(By.id("searchHoliday")).click();
+	      
+	       
+	}
+	
+	public static void vacationListing(WebDriver driver)
+	{
+		// selecting the deal "Discover the Magic of Dubai & Abu Dhabi: A Luxury Tour Experience"
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	       List<WebElement> wb11 = driver.findElements(By.className("holilist-content"));
+	       //System.out.println(wb1.getSize());
+	       for(int i=0; i<wb11.size();i++)
+	       {
+	    	   //System.out.println("value of i: "+i);
+	    	   String hol = wb11.get(i).getText();
+	    	   if(hol.contains("Discover the Magic of Dubai & Abu Dhabi: A Luxury Tour Experience"))
+	    	   {
+	    		 // Thread.sleep(5000);
+	    		   
+	    		   WebElement el = driver.findElements(By.xpath("//button[@type='submit']")).get(i);
+	    		   Actions act = new Actions(driver);
+	    		   act.moveToElement(el).click().perform();
+	    		   break;
+	    	   }
+	    	   //System.out.println(hol);
+	    	   
+	       }
 	}
 
 	
